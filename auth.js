fcc73e92e57e61cff2ac6d82f235dc33c100604a
yarn <yarnnd@gmail.com>
2016-02-14 03:36:16 -0800

    use config for some things

--- auth.js
+++ auth.js
@@ -2,34 +2,34 @@ var fs = require('fs');
 var auth_keys = JSON.parse(fs.readFileSync('./db/auth.json', 'utf8'));
 
 middleware = function(req, res, next){
-  var token = false
+  var token = false;
   if(req.headers.authorization){
-    token = req.headers.authorization
-    delete req.headers.authorization
+    token = req.headers.authorization;
+    delete req.headers.authorization;
   }else if(req.body.auth){
-    token = req.body.auth
-    delete req.body.auth
+    token = req.body.auth;
+    delete req.body.auth;
   }
   
-  var user = {}
-  user.auth = false
+  var user = {};
+  user.auth = false;
   if(!token){
-    user.auth = false
+    user.auth = false;
   }else{
     if(auth_keys[token]){
-        user.auth = auth_keys[token]
+        user.auth = auth_keys[token];
     }else{
-        user.auth = false
+        user.auth = false;
     }
   }
-  if(user.auth == false){
-    user.auth.view = []
-    user.auth.edit = []
-    user.auth.admin = false
+  if(user.auth === false){
+    user.auth.view = [];
+    user.auth.edit = [];
+    user.auth.admin = false;
   }
   
-  req.user = user
-  next()
-}
+  req.user = user;
+  next();
+};
 
-module.exports = {middleware:middleware}
+module.exports = {middleware:middleware};
--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-var path = require('path')
+var path = require('path');
 
-var express = require('express')
+var express = require('express');
 var Router = express.Router;
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+
+var config = require('config');
 
 /*var Db = require('tingodb')().Db,
     assert = require('assert');
 
 var db = new Db('./db/test', {});*/
 
-var tungus = require('tungus')
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema;
+var tungus = require('tungus');
+var mongoose = require('mongoose');
 var mongooseHidden = require('mongoose-hidden')();
 
-mongoose.connect('tingodb://./db/test', function (err) {
+mongoose.connect(config.get("uri"), function (err) {
   if(err) {
     console.log('connection error', err);
   } else {
     console.log('connection successful');
   }
-})
+});
 
 var app = express();
 
@@ -31,22 +32,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var auth = require('./auth.js')
-app.use(auth.middleware)
+var auth = require('./auth.js');
+app.use(auth.middleware);
 
-var router = require('./routes/pages.js')
-app.use("/pages", router)
+var router = require('./routes/pages.js');
+app.use("/pages", router);
 
-router = require('./routes/series.js')
-app.use("/series", router)
+router = require('./routes/series.js');
+app.use("/series", router);
 
 module.exports = app;
 
-//var debug = require('debug')('todoApp');
-
-app.set('port', process.env.PORT || 3000);
+app.set('port', config.get('port'));
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
-  //debug('Express server listening on port ' + server.address().port);
 });
