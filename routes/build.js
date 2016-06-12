7c7dbd69710e6783a44aa46b561b5a8d1b4a4a78
Yarn <yarnnd@gmail.com>
2016-06-11 20:35:33 -0700

    improve output of build command

--- routes/build.js
+++ routes/build.js
@@ -12,19 +12,18 @@ var active = false;
 router.get("/", function(req, res, next){
   "use strict";
   if(active){
-    res.end("no");
+    res.end("Already running");
     return;
   }
-  res.end("k");
   
   if(req.user.auth){
     if(config.has('build.type')){
       var type = config.get('build.type');
       if(type == "exec" && config.has('build.cmd')){
         var cmd = config.get('build.cmd');
-        var options = {}
+        var options = {};
         if(config.has('build.cwd')){
-          options.cwd = config.get('build.cwd')
+          options.cwd = config.get('build.cwd');
         }
         options.detatched = true;
         
@@ -37,9 +36,11 @@ router.get("/", function(req, res, next){
         active = true;
         p.stdout.on('data', (data) => {
           console.log("out", data.toString());
+          res.write(data.toString());
         });
         p.stderr.on('data', (data) => {
           console.log("err", data.toString());
+          res.write("stderr\n");
         });
         p.on('close', (code) => {
           console.log("close", code);
@@ -47,9 +48,11 @@ router.get("/", function(req, res, next){
         p.on('exit', (code) => {
           console.log('Exit', code);
           active = false;
+          res.end("End")
         });
         p.on('error', (err) => {
           console.log('Failed to start child process.', err);
+          res.end("Error");
         });
       }
     }
