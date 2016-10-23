e36dd756529d8861e62f8ec7820fae30b195f483
Yarn <yarnnd@gmail.com>
2016-10-22 19:30:17 -0700

    add deploy endpoint

--- routes/build.js
+++ routes/build.js
@@ -48,13 +48,67 @@ router.get("/", function(req, res, next){
         p.on('exit', (code) => {
           console.log('Exit', code);
           active = false;
-          res.end("End")
+          res.end("End");
         });
         p.on('error', (err) => {
           console.log('Failed to start child process.', err);
           res.end("Error");
         });
       }
+    }
+  }
+});
+
+router.get("/deploy", function(req, res, next){
+  "use strict";
+  if(active){
+    res.end("Already running");
+    return;
+  }
+  
+  if(req.user.auth.admin){
+    res.status(401)
+    res.end()
+    return
+  }
+  
+  if(config.has('deploy.type')){
+    var type = config.get('deploy.type');
+    if(type == "exec" && config.has('deploy.cmd')){
+      var cmd = config.get('deploy.cmd');
+      var options = {};
+      if(config.has('deploy.cwd')){
+        options.cwd = config.get('deploy.cwd');
+      }
+      options.detatched = true;
+      
+      var p;
+      if(config.has('deploy.args')){
+        p = spawn(cmd, config.get('deploy.args'), options);
+      }else{
+        p = spawn(cmd, [], options);
+      }
+      active = true;
+      p.stdout.on('data', (data) => {
+        console.log("out", data.toString());
+        res.write(data.toString());
+      });
+      p.stderr.on('data', (data) => {
+        console.log("err", data.toString());
+        res.write("stderr\n");
+      });
+      p.on('close', (code) => {
+        console.log("close", code);
+      });
+      p.on('exit', (code) => {
+        console.log('Exit', code);
+        active = false;
+        res.end("End");
+      });
+      p.on('error', (err) => {
+        console.log('Failed to start child process.', err);
+        res.end("Error");
+      });
     }
   }
 });
